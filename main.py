963d0a9334e9ffe5d84d2580a3c2e6d6fcf73138
ProCoderLol <86125707+ProCoderLol@users.noreply.github.com>
2021-08-26 10:55:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import speech_recognition as speech
+import pyttsx3
+import pywhatkit
+import pyjokes
+import datetime
+
+listener = speech.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def use_command():
+    try:
+        with speech.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+    except:
+        pass
+    return command
+
+
+while True:
+    command = use_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        speak('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        speak('Current time is ' + time)
+    elif 'joke' in command:
+        speak(pyjokes.get_joke())
+    else:
+        speak('Please say the command again.')
